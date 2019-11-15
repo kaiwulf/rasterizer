87e3097d3369ec8340f85f9e9c3daef257b035a9
kaiwulf <eigenspaces@gmail.com>
2019-11-15 13:34:49 -0500

    drawing primitives to the screen

---
+++ draw_line/x11_draw_line.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Intrinsic.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+void event_loop();
+const char *event_name(int e_type);
+void create_window();
+void event_notification();
+void draw_line(int, int, int, int);
